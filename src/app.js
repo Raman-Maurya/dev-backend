bde4a862b654c0701c0fa13535f15c3b5e83634c
Raman-Maurya <raman.2005kumar@gmail.com>
2025-02-11 20:59:30 +0530

    devTinder Backend

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors  = require("cors")
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials:true,
+}));
+
+const authRouter = require("./routes/authRouter");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/requests");
+const userRouter = require("./routes/user");
+
+
+connectDB().then(()=>{
+    console.log("Connected to Database");
+    app.listen(7777,()=>{
+        console.log("Server is running on port 7777");
+    })
+
+}).catch((err)=>{
+    console.log("Error connecting to Database", err);
+})
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
+app.use("/",userRouter);
---
+++ src/models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Userschema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    lastName:{
+        type: String,
+    },
+    emailId:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true
+    },
+    age:{
+        type: Number,
+        required: true,
+        min: 18,
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    photoUrl:{
+        type: String,
+        default: "www.myprofilephoto.com"
+    },
+    Skills:{
+        type: [String],
+    },
+    about:{
+        type: String,
+        maxlength: 200,
+        default:"About Section"
+    },
+},{timestamps: true});
+
+Userschema.methods.getJWT = async function(){
+    const user = this;
+   const token = await jwt.sign({_id:user._id}, "Signature#@12", {expiresIn: "7d"});
+   return token;
+    
+}
+
+module.exports = mongoose.model("User", Userschema);
