67306cd905f907e0eb77e304e45ba57bdf4f00be
Raman-Maurya <raman.2005kumar@gmail.com>
2025-04-14 10:40:52 +0530

    enhanced UI

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+// require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const connectDB = async() =>{
     await mongoose.connect(process.env.MONGO_URI)
 }
--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,5 +1,6 @@
 const express = require("express");
-require('dotenv').config();
+// require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const { jwtAuth } = require("../middleware/validateData");
 const paymentRouter = express.Router();
 const Payment = require("../models/payment");
