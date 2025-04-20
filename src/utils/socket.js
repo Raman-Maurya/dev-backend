8b4ecbd6840d3ec570b26c12fdcbf65cd3d8db40
Raman Maurya <159642215+Raman-Maurya@users.noreply.github.com>
2025-04-20 16:00:11 +0530

    Update socket.js for chat error

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -13,7 +13,8 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://dev-frontend-final.onrender.com",
+      credentials: true,
     },
   });
 
